7a06f68e666efd17139bd1b620e8cd1b510db231
Rodrigo Braz Carneiro <braz.rodrigo94@gmail.com>
2021-02-27 00:20:58 -0300

    Adicionado teste para lances com ordem decrescente

--- src/test/java/br/com/zup/testes/TesteAvaliador.java
+++ src/test/java/br/com/zup/testes/TesteAvaliador.java
@@ -102,4 +102,22 @@ public class TesteAvaliador {
         assertEquals(100000.0, leiloeiro.getMaiorLance(), 0.00001);
         assertEquals(10.0, leiloeiro.getMenorLance(), 0.00001);
     }
+
+    @Test
+    public void deveEntenderLeilaoComLancesEmOrdemDecrescente() {
+        Usuario joao = new Usuario("Joao"); 
+        Usuario maria = new Usuario("Maria"); 
+        Leilao leilao = new Leilao("Playstation 3 Novo");
+
+        leilao.propoe(new Lance(joao,400.0));
+        leilao.propoe(new Lance(maria,300.0));
+        leilao.propoe(new Lance(joao,200.0));
+        leilao.propoe(new Lance(maria,100.0));
+
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+
+        assertEquals(400.0, leiloeiro.getMaiorLance(), 0.0001);
+        assertEquals(100.0, leiloeiro.getMenorLance(), 0.0001);
+    }
 }
